5c1c6fb1c26e02180d020bd6ae13cfb6d811c18e
Doggie3 <101623164+Doggie3@users.noreply.github.com>
2024-09-06 16:37:18 +0800

    基础

---
+++ main.js
@@ -0,0 +1,31 @@
+// main.js
+function loadSidebar() {
+    fetch('/sidebar.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('sidebar-container').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading the sidebar:', error);
+        });
+}
+function loadIndex() {
+    fetch('/catalog.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('catalog-container').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading the catalog:', error);
+        });
+}
