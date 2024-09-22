d845fa5ce8951a6102c577404dbef64298c15d17
Doggie3 <101623164+Doggie3@users.noreply.github.com>
2024-09-22 21:24:23 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // main.js
 function loadSidebar() {
-    fetch('/sidebar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('sidebar-container').innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading the sidebar:', error);
-        });
+    if(Document.documentElement.scrollWidth>20)
+    {
+        fetch('/sidebar.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById('sidebar-container').innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Error loading the sidebar:', error);
+            });
+    }
 }
 function loadIndex() {
     fetch('/catalog.html')
